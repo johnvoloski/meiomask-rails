fae638a37527244484213e908ab6b2c3a0f48aa9
John Voloski <johnvoloski@cwi.com.br>
2014-05-23 15:11:31 -0300

    configure coveralls

--- Rakefile
+++ Rakefile
@@ -2,4 +2,4 @@ require 'bundler/setup'
 
 Dir[File.join(File.dirname(__FILE__), 'tasks/*.rake')].each { |f| load f }
 
-task default: [:spec]
+task default: [:spec, 'coveralls:push']
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@
 require 'bundler'
 Bundler.require :default, :test
 
+require 'coveralls'
+Coveralls.wear!
+
 require 'rspec'
 RSpec.configure do |config|
   config.color_enabled = true
